21ddc912185cb9200a399392f61085fc8bc6fc74
OBreaux1 <obreaux1@gmail.com>
2016-01-09 15:50:35 -0500

    Add GoogleMaps Pod and sample code

--- oldNYC-iOS/ViewController.swift
+++ oldNYC-iOS/ViewController.swift
@@ -2,17 +2,30 @@
 //  ViewController.swift
 //  oldNYC-iOS
 //
-//  Created by Orian Breaux on 1/9/16.
+//  Created by Orian Breaux and Christina Leuci on 1/9/16.
 //  Copyright © 2016 OldNYC. All rights reserved.
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let camera = GMSCameraPosition.cameraWithLatitude(-33.868,
+            longitude:151.2086, zoom:6)
+        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera:camera)
+        
+        let marker = GMSMarker()
+        marker.position = camera.target
+        marker.snippet = "Hello World"
+        marker.appearAnimation = kGMSMarkerAnimationPop
+        marker.map = mapView
+        
+        self.view = mapView
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
