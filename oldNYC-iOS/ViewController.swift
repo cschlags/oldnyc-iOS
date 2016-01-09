b300c53a0cd91f6dbd72fb8bb751f30d1f3f8258
OBreaux1 <obreaux1@gmail.com>
2016-01-09 15:18:58 -0500

    format change

---
+++ oldNYC-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  oldNYC-iOS
+//
+//  Created by Orian Breaux on 1/9/16.
+//  Copyright © 2016 OldNYC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
