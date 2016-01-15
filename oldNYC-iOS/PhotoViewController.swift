6b4e3207c456aa2fb4ce52681a0a945d9944d69c
Christina Leuci <christinaleuci@gmail.com>
2016-01-15 16:41:07 -0500

    added the photo collection view controller, changed photoicon and mapicon to a larger size

--- oldNYC-iOS/PhotoViewController.swift
+++ oldNYC-iOS/PhotoViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class PhotoViewController: UIViewController {
+class PhotoViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
