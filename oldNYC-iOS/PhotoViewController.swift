2403dd84b9b31e2504382f994390a6140c982122
Orian Breaux <obreaux1@gmail.com>
2016-03-13 21:00:30 -0400

    Set up bridging header for NYTPhotoViewer

---
+++ oldNYC-iOS/PhotoViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoViewController.swift
+//  oldNYC-iOS
+//
+//  Created by Orian Breaux and Christina Leuci.
+//  Copyright © 2016 OldNYC. All rights reserved.
+//
+
+import UIKit
+
+
+class PhotoViewController: UIViewController, NYTPhotosViewControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
