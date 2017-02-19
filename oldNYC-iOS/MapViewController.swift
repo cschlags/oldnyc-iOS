d9e2b2a958dd689a9fef40591335413cacb52a16
Orian Breaux <obreaux1@gmail.com>
2017-02-18 22:05:45 -0500

    Increment build number

--- oldNYC-iOS/MapViewController.swift
+++ oldNYC-iOS/MapViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import CoreLocation
 import Mapbox
 import SwiftyJSON
+
 // FIXME: comparison operators with optionals were removed from the Swift Standard Libary.
 // Consider refactoring the code to use the non-optional operators.
 fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
